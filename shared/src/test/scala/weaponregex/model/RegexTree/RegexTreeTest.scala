c34aefd6c7f31fce1ff2760cb81f813345b02d37
wijtse <wijtse@rekkerit.nl>
2020-11-16 17:32:39 +0100

    Add test

--- shared/src/test/scala/weaponregex/model/RegexTree/RegexTreeTest.scala
+++ shared/src/test/scala/weaponregex/model/RegexTree/RegexTreeTest.scala
@@ -1,9 +1,19 @@
-package weaponregex
+package weaponregex.model.RegexTree
 
 import weaponregex.model._
 import weaponregex.model.RegexTree._
 
-class RegexTreeSuite extends munit.FunSuite {
+class RegexTreeTest extends munit.FunSuite {
+  var locStub: Location = Location(Position(0, 0), Position(0, 1))
+
+  test("Character build") {
+    val node1 = Character('a', locStub)
+    assertEquals("a", node1.build)
+
+    val node2 = Character('b', locStub)
+    assertEquals("b", node2.build)
+  }
+
   test("RegexTree build") {
     val pattern: String = """^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$"""
     val loc = Location(Position(0, 0), Position(0, 1))
